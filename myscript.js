497aeb1b656cd2eee877a37c4fdd5060a843b3ed
Mihailo Isakov <mihailo@protonmail.ch>
2016-03-03 01:05:44 +0100

    Working button insertion

--- myscript.js
+++ myscript.js
@@ -1,19 +1,18 @@
 // Checking page title
 alert('1');
 
-chrome.webNavigation.onCompleted.addListener(function(details) {
+//chrome.webNavigation.onCompleted.addListener(function(details) {
     //chrome.tabs.executeScript(details.tabId, {
     //code: ' if (document.body.innerText.indexOf("Cat") !=-1) {' +
     //'     alert("Cat not found!");' +
     //' }'
     //});
-    alert('load');
-}, {
-    url: [{
-            // Runs on example.com, example.net, but also example.foo.com
-            hostContains: '.example.'
-    },]
-});
+//}, {
+    //url: [{
+            //// Runs on example.com, example.net, but also example.foo.com
+            //hostContains: '.example.'
+    //},]
+//});
 //Creating Elements
 var btn = document.createElement("BUTTON");
 var t = document.createTextNode("CLICK ME");
