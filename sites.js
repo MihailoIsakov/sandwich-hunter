e92bf3f980825e880529014b8482c011908a1b21
Mihailo Isakov <mihailo@protonmail.ch>
2016-03-14 02:27:07 +0100

    Forgot to remove sites

--- sites.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    if ($(location).attr('href').indexOf("blic.rs") > -1) 
-        blic_ready();
-
-    if ($(location).attr('href').indexOf("b92.net") > -1) 
-        b92_ready();
-});
-
