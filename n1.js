b89a189f1d46724cc2cd1f344ff785e4470a9f3c
Mihailo Isakov <mihailo@protonmail.ch>
2016-03-19 18:42:08 +0100

    Working N1 port

--- n1.js
+++ n1.js
@@ -8,15 +8,19 @@ chrome.runtime.sendMessage({method: "getId"}, function(response) {
 
 // Adds a than you message after the bot or not bot button is clicked
 thank = function(element, data) {
-    thanks = $(element).closest(".k_nForum_Actions").find(".bot_count").text(data.bot_count);
-    thanks = $(element).closest(".k_nForum_Actions").find(".not_count").text(data.not_count);
+    thanks = $(element).closest(".comment-unit").find(".bot_count").text(' ' + data.bot_count);
+    thanks = $(element).closest(".comment-unit").find(".bot_count").text(' ' + data.bot_count);
+    thanks = $(element).closest(".comment-unit").find(".bot_count").css('padding-left', '4px');
+    thanks = $(element).closest(".comment-unit").find(".bot_count").css('padding-left', '4px');
+    //thanks = $(element).closest(".not_count").find(".not_count").text(data.not_count);
+    //thanks = $(element).closest(".not_count").find(".not_count").text(data.not_count);
 };
 
 send_data = function(data, thank_button) {
     $.ajax({
         type: "GET",
-        //url: "http://46.101.172.101/n1",
-        url: "http://0.0.0.0:5000/n1",
+        url: "http://46.101.172.101/n1",
+        //url: "http://0.0.0.0:5000/n1",
         data: data,
         success: function(response) {
             thank(thank_button, response);
@@ -28,15 +32,16 @@ send_data = function(data, thank_button) {
 //   - the authors username, timestamp, votes...
 collect_data = function(element) {
     element = element.closest(".comment-unit").first();
+    id = '';
     if(element.find(".js_replyComment").length) {
-        var id = element.find(".js_replyComment").attr('data-comm-id');
+        id = element.find(".js_replyComment").attr('data-comm-id');
     } else { 
-        var id = element.find(".js_toggleLike").attr('id');
+        id = element.find(".js_toggleLike").attr('id');
     }
     var link = $(location).attr('href');
     var author = element.find(".comment-author").text().trim();
     //var parent_author = element.find(".k_parentAuthor").text().trim();
-    var parent_author = ''
+    var parent_author = '';
     var comment = element.find(".comment-text").text().trim();
     var vote_count = '';
     var upvotes = element.find(".comment-likes .js_likeNum").text().trim();
@@ -64,8 +69,8 @@ $(document).ready(function() {
 
     // Add the buttons
     $(".comment-likes").each(function() {
-        var bot_button = $('<span class="btn btn-grey no-translate-y bot_button"><span class="js_likeNum" >BOT!</span></span>');
-        var not_button = $('<span class="btn btn-grey no-translate-y not_button"><span class="js_likeNum" >nije bot</span></span>');
+        var bot_button = $('<span class="btn btn-grey no-translate-y bot_button"><span class="js_likeNum" >BOT! <span class="bot_count"></span></span></span>');
+        var not_button = $('<span class="btn btn-grey no-translate-y not_button"><span class="js_likeNum" >nije bot <span class="bot_count"></span></span></span>');
 
         bot_button.insertBefore(this);
         not_button.insertBefore(this);
@@ -74,8 +79,8 @@ $(document).ready(function() {
     // On click bot button
     $(".bot_button").click(function() {
         // De-bold the other button and bold this one
-        $(this).parent().find(".not_button").css("font-weight", "normal");
-        $(this).css("font-weight", "bold");
+        $(this).parent().find(".not_button span").css("font-weight", "normal");
+        $(this).find("span").css("font-weight", "bold");
 
         // Set the comment background
         $(this).closest(".comment-unit").css("background", BOT_BACKGROUND);
@@ -87,8 +92,8 @@ $(document).ready(function() {
 
     $(".not_button").click(function() {
         // De-bold the other button and bold this one
-        $(this).parent().find(".bot_button").css("font-weight", "normal");
-        $(this).css("font-weight", "bold");
+        $(this).parent().find(".bot_button span").css("font-weight", "normal");
+        $(this).find("span").css("font-weight", "bold");
 
         // Set the comment background
         $(this).closest(".comment-unit").css("background", NOT_BACKGROUND);
